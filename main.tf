526c82f1ac5e64d109c60e1363bc84b306080da0
mahesh yadav <devopsmahesh.it@gmail.com>
2023-06-22 20:04:08 +0530

    feat: inital commit

--- main.tf
+++ main.tf
@@ -1,6 +1,68 @@
-# ------------------------------------------------------------------------------
-# Resources
-# ------------------------------------------------------------------------------
-locals {
+##-----------------------------------------------------------------------------
+## Labels module callled that will be used for naming and tags.
+##-----------------------------------------------------------------------------
+module "labels" {
+  source      = "git::https://github.com/terraform-do-modules/terraform-digitalocean-labels.git?ref=internal-426m"
+  name        = var.name
+  environment = var.environment
+  managedby   = var.managedby
   label_order = var.label_order
+}
+
+##---------------------------------------------------------------------------------------------------------------------------
+#Description : Provides a DigitalOcean Kubernetes cluster resource. This can be used to create, delete, and modify clusters.
+##---------------------------------------------------------------------------------------------------------------------------
+#tfsec:ignore:digitalocean-compute-kubernetes-auto-upgrades-not-enabled  ### by default we disable auto-upgrades, if it enables auto-upgrades, maybe create an issue in the cluster.
+#tfsec:ignore:digitalocean-compute-surge-upgrades-not-enabled ### by default we disable surge upgrades, if it enables surge upgrades, maybe create an issue in the cluster.
+resource "digitalocean_kubernetes_cluster" "main" {
+  count                = var.enabled ? 1 : 0
+  name                 = format("%s-cluster", module.labels.id)
+  region               = var.region
+  version              = var.cluster_version
+  vpc_uuid             = var.vpc_uuid
+  auto_upgrade         = var.auto_upgrade
+  surge_upgrade        = var.surge_upgrade
+  ha                   = var.ha
+  registry_integration = var.registry_integration
+
+  dynamic "node_pool" {
+    for_each = var.default_node_pool
+    content {
+      name       = lookup(node_pool.value, "name", "default-node-pool")
+      size       = lookup(node_pool.value, "size", "s-1vcpu-2gb")
+      node_count = lookup(node_pool.value, "node_count", 1)
+      auto_scale = lookup(node_pool.value, "auto_scale", true)
+      min_nodes  = lookup(node_pool.value, "min_nodes", 1)
+      max_nodes  = lookup(node_pool.value, "max_nodes", 2)
+      tags       = lookup(node_pool.value, "tags", null)
+      labels     = lookup(node_pool.value, "labels", null)
+    }
+  }
+
+  dynamic "maintenance_policy" {
+    for_each = var.auto_upgrade ? [1] : []
+    content {
+      day        = var.maintenance_policy.day
+      start_time = var.maintenance_policy.start_time
+    }
+  }
+
+  tags = var.tags
+}
+
+##--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Description : Provides a DigitalOcean Kubernetes node pool resource. While the default node pool must be defined in the digitalocean_kubernetes_cluster resource, this resource can be used to add additional ones to a cluster.
+##--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+resource "digitalocean_kubernetes_node_pool" "main" {
+  for_each = var.enabled ? var.node_pools : {}
+
+  cluster_id = join("", digitalocean_kubernetes_cluster.main[*].id)
+  name       = lookup(each.value, "name", "node-pool")
+  size       = lookup(each.value, "size", "s-1vcpu-2gb")
+  node_count = lookup(each.value, "node_count", 1)
+  auto_scale = lookup(each.value, "auto_scale", true)
+  min_nodes  = lookup(each.value, "min_nodes", 1)
+  max_nodes  = lookup(each.value, "max_nodes", 2)
+  tags       = lookup(each.value, "tags", null)
+  labels     = lookup(each.value, "labels", null)
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,51 @@
 # ------------------------------------------------------------------------------
 # Outputs
 # ------------------------------------------------------------------------------
-output "label_order" {
-  value       = local.label_order
-  description = "Label order."
+output "id" {
+  value       = digitalocean_kubernetes_cluster.main[*].id
+  description = "A unique ID that can be used to identify and reference a Kubernetes cluster."
+}
+output "cluster_subnet" {
+  value       = digitalocean_kubernetes_cluster.main[*].cluster_subnet
+  description = "The range of IP addresses in the overlay network of the Kubernetes cluster."
+}
+output "service_subnet" {
+  value       = digitalocean_kubernetes_cluster.main[*].service_subnet
+  description = " The range of assignable IP addresses for services running in the Kubernetes cluster."
+}
+output "ipv4_address" {
+  value       = digitalocean_kubernetes_cluster.main[*].ipv4_address
+  description = "The public IPv4 address of the Kubernetes master node. This will not be set if high availability is configured on the cluster (v1.21+)"
+}
+output "endpoint" {
+  value       = digitalocean_kubernetes_cluster.main[*].endpoint
+  description = "The base URL of the API server on the Kubernetes master node."
+}
+output "status" {
+  value       = digitalocean_kubernetes_cluster.main[*].status
+  description = "A string indicating the current status of the cluster. Potential values include running, provisioning, and errored."
+}
+output "created_at" {
+  value       = digitalocean_kubernetes_cluster.main[*].created_at
+  description = "The date and time when the Kubernetes cluster was created."
+}
+output "updated_at" {
+  value       = digitalocean_kubernetes_cluster.main[*].updated_at
+  description = "The date and time when the Kubernetes cluster was last updated."
+}
+output "auto_upgrade" {
+  value       = digitalocean_kubernetes_cluster.main[*].auto_upgrade
+  description = "A boolean value indicating whether the cluster will be automatically upgraded to new patch releases during its maintenance window."
+}
+output "default_node_pool_id" {
+  value       = digitalocean_kubernetes_cluster.main[*].node_pool
+  description = "A unique ID that can be used to identify and reference the node pool."
+}
+output "urn" {
+  value       = digitalocean_kubernetes_cluster.main[*].urn
+  description = "The uniform resource name (URN) for the Kubernetes cluster."
+}
+output "maintenance_policy_day" {
+  value       = digitalocean_kubernetes_cluster.main[*].maintenance_policy
+  description = "A block representing the cluster's maintenance window. Updates will be applied within this window."
 }

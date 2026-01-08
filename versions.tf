689a0a43d0ac919648175f872f56ecc113c2a8e9
abhinav <abhinav.shekhawat@clouddrove.com>
2026-01-08 18:22:11 +0530

    fix: updated workflow files & versions.tf

--- versions.tf
+++ versions.tf
@@ -6,5 +6,9 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = ">= 2.28.1"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.3"
+    }
   }
 }

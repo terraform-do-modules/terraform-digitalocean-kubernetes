45a282d77fffd397ca4f499e252a6291cb87e22f
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-02-06 07:24:27 -0500

    ⬆️ upgrade: update examples to use provider >= 2.70.0

--- _examples/complete/versions.tf
+++ _examples/complete/versions.tf
@@ -1,10 +1,10 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.4.6"
+  required_version = ">= 1.5.4"
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = ">= 2.28.1"
+      version = ">= 2.70.0"
     }
   }
 }

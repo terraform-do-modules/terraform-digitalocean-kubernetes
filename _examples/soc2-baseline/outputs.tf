2c226e6cfa6293aff5a1cec5bbb5e6757630615f
anmolnagpal <anmol@clouddrove.com>
2026-02-16 03:02:51 +0000

    fix(example): align soc2 outputs with module output names

--- _examples/soc2-baseline/outputs.tf
+++ _examples/soc2-baseline/outputs.tf
@@ -1,14 +1,9 @@
-output "cluster_name" {
-  description = "Kubernetes cluster name"
-  value       = module.cluster.cluster_name
+output "cluster_id" {
+  description = "Kubernetes cluster ID"
+  value       = module.cluster.id
 }
 
 output "cluster_endpoint" {
   description = "Kubernetes API endpoint"
-  value       = module.cluster.cluster_endpoint
-}
-
-output "cluster_id" {
-  description = "Kubernetes cluster ID"
-  value       = module.cluster.cluster_id
+  value       = module.cluster.endpoint
 }
